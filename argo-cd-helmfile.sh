d27fd83d971083546c1301ccca11a6a4a2ed45d9
Duc Minh Le <duc.lm@teko.vn>
2021-11-16 15:07:14 +0700

    update script

--- argo-cd-helmfile.sh
+++ argo-cd-helmfile.sh
@@ -247,14 +247,14 @@ case $phase in
       INTERNAL_HELM_TEMPLATE_OPTIONS="${INTERNAL_HELM_TEMPLATE_OPTIONS} ${INTERNAL_HELM_API_VERSIONS}"
     fi
 
-    check_raw=`find **/ -name 'namespace.yaml.raw'`
-    if [ -z "$check_raw" ]; then
-      echo "No namespace.yaml.raw found"
-      find **/ -name 'namespace.yaml' -exec cp {} {}.raw \;
-    else
-      echo "namespace.yaml.raw found"
-      find **/ -name 'namespace.yaml.raw' -exec sh -c 'cp {} $(dirname "{}")/namespace.yaml' \;
-    fi
+    # check_raw=`find **/ -name 'namespace.yaml.raw'`
+    # if [ -z "$check_raw" ]; then
+    #   echo "No namespace.yaml.raw found"
+    #   find **/ -name 'namespace.yaml' -exec cp {} {}.raw \;
+    # else
+    #   echo "namespace.yaml.raw found"
+    #   find **/ -name 'namespace.yaml.raw' -exec sh -c 'cp {} $(dirname "{}")/namespace.yaml' \;
+    # fi
 
     find **/ -name 'namespace.yaml' -exec sed -i "s/namespace: /namespace: ${ENVIRONMENT}-/g" {} \;
 
