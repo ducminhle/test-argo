60323d43ef7565b3364b75a958bc66d1295ee713
Le Minh Duc <minhducle.it@gmail.com>
2021-11-18 13:42:37 +0700

    update script

--- argo-cd-helmfile.sh
+++ argo-cd-helmfile.sh
@@ -150,6 +150,17 @@ case $phase in
 
     find **/ -name 'helmfile.yaml' -exec sed -i "s/forceNamespace: /forceNamespace: ${ENVIRONMENT}-/g" {} \;
 
+    check_raw=`find **/ -name 'namespace.yaml.raw'`
+    if [ -z "$check_raw" ]; then
+      echo "No namespace.yaml.raw found"
+      find **/ -name 'namespace.yaml' -exec cp {} {}.raw \;
+    else
+      echo "namespace.yaml.raw found"
+      find **/ -name 'namespace.yaml.raw' -exec sh -c 'cp {} $(dirname "{}")/namespace.yaml' \;
+    fi
+
+    find **/ -name 'namespace.yaml' -exec sed -i "s/namespace: /namespace: ${ENVIRONMENT}-/g" {} \;
+
     # ensure dir(s)
     # rm -rf "${HELM_HOME}"
     if [[ ! -d "${HELM_HOME}" ]]; then
@@ -246,17 +257,6 @@ case $phase in
       done
       INTERNAL_HELM_TEMPLATE_OPTIONS="${INTERNAL_HELM_TEMPLATE_OPTIONS} ${INTERNAL_HELM_API_VERSIONS}"
     fi
-
-    check_raw=`find **/ -name 'namespace.yaml.raw'`
-    if [ -z "$check_raw" ]; then
-      echo "No namespace.yaml.raw found"
-      find **/ -name 'namespace.yaml' -exec cp {} {}.raw \;
-    else
-      echo "namespace.yaml.raw found"
-      find **/ -name 'namespace.yaml.raw' -exec sh -c 'cp {} $(dirname "{}")/namespace.yaml' \;
-    fi
-
-    find **/ -name 'namespace.yaml' -exec sed -i "s/namespace: /namespace: ${ENVIRONMENT}-/g" {} \;
 
     ${helmfile} \
       template \
